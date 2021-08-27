a51bb7ff6aff6601f310a41349a06d382cbc441d
Ross Brower <Ross.Brower@edgile.com>
2021-08-26 21:03:04 -0700

    support for IDP initiated SAML templating.

--- scripts/Build-CustomPolicies.ps1
+++ scripts/Build-CustomPolicies.ps1
@@ -15,13 +15,13 @@
    1. appsettings.json file exists in proper format        
 #>
 param(
-#File Path containing the appsettings.json and the XML policy files
-[Parameter(Mandatory = $true)]
-[string]
-$FilePath
+    #File Path containing the appsettings.json and the XML policy files
+    [Parameter(Mandatory = $true)]
+    [string]
+    $FilePath
 )
 
-try{
+try {
     #Check if appsettings.json is existed under for root folder        
     $AppSettingsFile = Join-Path $FilePath "appsettings.json"
 
@@ -36,54 +36,102 @@ try{
     $EnvironmentsRootPath = Join-Path $FilePath "Environments"
 
     #Ensure environments folder exists
-    if((Test-Path -Path $EnvironmentsRootPath -PathType Container) -ne $true)
-    {
+    if ((Test-Path -Path $EnvironmentsRootPath -PathType Container) -ne $true) {
         New-Item -ItemType Directory -Force -Path $EnvironmentsRootPath | Out-Null
     }                                    
 
+    #temp files that will be deleted at the end.
+    $tmpFiles = @{};
+
     #Iterate through environments  
-    foreach($entry in $AppSettingsJson.Environments)
-    {
+    foreach ($entry in $AppSettingsJson.Environments) {
         Write-Verbose "ENVIRONMENT: $($entry.Name)"
 
-        if($null -eq $entry.PolicySettings){
+        if ($null -eq $entry.PolicySettings) {
             Write-Error "Can't generate '$($entry.Name)' environment policies. Error: Accepted PolicySettings element is missing. You may use old version of the appSettings.json file. For more information, see [App Settings](https://github.com/yoelhor/aad-b2c-vs-code-extension/blob/master/README.md#app-settings)"
         }
         else {
             $environmentRootPath = Join-Path $EnvironmentsRootPath $entry.Name
 
-            if((Test-Path -Path $environmentRootPath -PathType Container) -ne $true)
-            {
+            if ((Test-Path -Path $environmentRootPath -PathType Container) -ne $true) {
                 New-Item -ItemType Directory -Force -Path $environmentRootPath | Out-Null
             }    
 
-            #Iterate through the list of settings
-            foreach($file in $XmlPolicyFiles)
-            {
+            #Iterate through the list of files and replace environment settings
+            foreach ($file in $XmlPolicyFiles) {
                 Write-Verbose "FILE: $($entry.Name) - $file"
 
-                $policContent = Get-Content (Join-Path $FilePath $file) | Out-String
+                $policContent = Get-Content (Join-Path $FilePath $file.Name) | Out-String
 
                 #Replace the tenant name
                 $policContent = $policContent -replace "\{Settings:Tenant\}", $entry.Tenant
 
                 #Replace the rest of the policy settings
                 $policySettingsHash = @{}; #ugly hash conversion from psobject so we can access json properties via key
-                $entry.PolicySettings.psobject.properties | ForEach-Object{ $policySettingsHash[$_.Name] = $_.Value }
-                foreach($key in $policySettingsHash.Keys)
-                {
+                $entry.PolicySettings.psobject.properties | ForEach-Object { $policySettingsHash[$_.Name] = $_.Value }
+                foreach ($key in $policySettingsHash.Keys) {
                     Write-Verbose "KEY: $key VALUE: $($policySettingsHash[$key])"
                     $policContent = $policContent -replace "\{Settings:$($key)\}", $policySettingsHash[$key]
                 }
 
                 #Save the  policy
-                $policContent | Set-Content ( Join-Path $environmentRootPath $file )            
+                $policContent | Set-Content ( Join-Path $environmentRootPath $file.Name)
+
+            }
+
+            #iterate through identity providers and produce policies based on the template
+            foreach ($idp in $entry.IdentityProviders) {                
+                $templatePath = Join-Path $environmentRootPath $idp.Template;                
+                #add idp template file to cleanup list since it is not valid on its own.
+                $tmpFiles[$templatePath] = $null;
+                #grab the already written template so environment settings are propagated.
+                $idpContent = Get-Content $templatePath | Out-String;
+                #Replace the Identity Provider name
+                $idpContent = $idpContent -replace "\{IdentityProvider:Name\}", $idp.Name
+                #Replace the rest of the policy settings
+                $idpSettingsHash = @{}; #ugly hash conversion from psobject so we can access json properties via key
+                $idp.PolicySettings.psobject.properties | ForEach-Object { $idpSettingsHash[$_.Name] = $_.Value }
+                foreach ($key in $idpSettingsHash.Keys) {
+                    Write-Verbose "KEY: $key VALUE: $($idpSettingsHash[$key])"
+                    $idpContent = $idpContent -replace "\{IdentityProvider:$($key)\}", $idpSettingsHash[$key]
+                }                
+                #Save the policy by appending the idp name to the template file name.
+                $idpPolicyFileName = $idp.Template.Replace(".xml", $idp.Name + ".xml");
+                $idpContent | Set-Content (Join-Path $environmentRootPath $idpPolicyFileName)  
+
+                #iterate through applications and produce policies mapped to this idp
+                foreach ($app in $entry.Applications) {
+                    $appPath = Join-Path $environmentRootPath $app.Template; 
+                    #add app template file to cleanup list since it is not valid on its own.
+                    $tmpFiles[$appPath] = $null;
+                    #grab the already written template so environment settings are propagated.
+                    $appContent = Get-Content $appPath | Out-String;                    
+                    #Replace the Identity Provider name
+                    $appContent = $appContent -replace "\{IdentityProvider:Name\}", $idp.Name
+                    #Replace the Application name
+                    $appContent = $appContent -replace "\{Application:Name\}", $app.Name
+
+                    #Replace the rest of the app settings
+                    $appSettingsHash = @{}; #ugly hash conversion from psobject so we can access json properties via key
+                    $app.PolicySettings.psobject.properties | ForEach-Object { $appSettingsHash[$_.Name] = $_.Value }
+                    foreach ($key in $appSettingsHash.Keys) {
+                        Write-Verbose "KEY: $key VALUE: $($appSettingsHash[$key])"
+                        $appContent = $appContent -replace "\{Application:$($key)\}", $appSettingsHash[$key]
+                    }                
+                    #Save the policy by appending the idp name to the template file name.
+                    $appPolicyFileName = $app.Template.Replace(".xml", $idp.Name + "_" + $app.Name + ".xml");
+                    $appContent | Set-Content ( Join-Path $environmentRootPath $appPolicyFileName )
+                }
+            }
+            foreach($key in $tmpFiles.keys)
+            {
+                Remove-Item $key;
             }
         }
 
         Write-Output "You policies successfully exported and stored under the Environment folder ($($entry.Name))."
     }
 }
-catch{
+catch {
     Write-Error $_
 }
